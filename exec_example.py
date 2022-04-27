8c4c9eb570e0c924f156d9d2b8f40acc811012d8
Feliú Sagols <fsagols@math.cinvestav.edu.mx>
2022-04-26 21:45:17 -0500

    exec_example.py modification

--- exec_example.py
+++ exec_example.py
@@ -6,16 +6,17 @@ April 5th, 2022
 CDMX
 """
 
+
 def exceptions():
     a = int(input("Type an integer number please >>> "))
     try:
-        print("The inverse is %f" % (1/a))
+        print("The inverse is %f" % (1 / a))
     except ZeroDivisionError as e:
         print("No puedes poner cero")
         raise NotImplementedError("No se qué hacer")
 
 
-exceptions()
+# exceptions()
 
 while True:
     program = input("Enter a python expression >>> ")
